bcb4039db80ea56bf03cdb96c9d2f17a17bcb15c
AmandaLBorges <amandalemesbor@gmail.com>
2015-04-06 10:41:07 -0300

    Create exercicio1.php
    Exercicio1.php

---
+++ php/exercicio1.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="pt-br">
+	<head>
+		<meta charset= "utf-8">
+		<title> Exemplo </title>
+	</head>
+	<body>
+		<?php
+		echo "Olá,Eu sou um script PHP!";
+		
+		$foo="0";
+		echo $foo;
+		echo "<br>";
+		 
+		$foo+=2;
+		echo $foo;
+		echo "<br>";
+		
+		$foo = $foo + 1.3;
+		echo $foo;
+		echo "<br>";
+		
+		$foo = 5 + "10 pequenos porcos";
+		echo $foo;
+		echo "<br>";
+		
+		$foo = 5 + "10 minusculos porcos";
+		echo $foo;
+		echo "<br>";
+		
+		$a = "car";
+		echo $a;
+		
+		$a[0] = "b";
+		echo $a;
+		
+		$foo = 10;
+		echo $foo;
+		
+		$bar = (boolean) $foo;
+		echo $foo;
+		echo "<br>";
+		
+		
+		 
+		?>
+	</body>
+</html>		
